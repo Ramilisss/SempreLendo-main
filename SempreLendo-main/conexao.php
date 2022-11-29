c5c09f1e0035a1c21ab30fc70b72dcbee106349e
KleyEsh <kleyslaeshily1@gmail.com>
2022-11-29 07:30:04 -0300

    Atualização

---
+++ SempreLendo-main/conexao.php
@@ -0,0 +1,8 @@
+<?php
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', 'Kleysl@2004');
+define('DB', 'login');
+
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possivel conectar');
+?>
